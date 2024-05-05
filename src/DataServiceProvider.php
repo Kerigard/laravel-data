bfbce301868ded6a1a149b47a81c4ab3c065fd29
Kerigard <2fenix7@gmail.com>
2024-05-05 13:44:54 +0300

    fix: make the service provider deferred

--- src/DataServiceProvider.php
+++ src/DataServiceProvider.php
@@ -2,10 +2,11 @@
 
 namespace Kerigard\LaravelData;
 
+use Illuminate\Contracts\Support\DeferrableProvider;
 use Illuminate\Support\ServiceProvider;
 use Kerigard\LaravelData\Commands\FillDatabaseData;
 
-class DataServiceProvider extends ServiceProvider
+class DataServiceProvider extends ServiceProvider implements DeferrableProvider
 {
     public function boot(): void
     {
@@ -14,5 +15,12 @@ class DataServiceProvider extends ServiceProvider
                 FillDatabaseData::class,
             ]);
         }
+    }
+
+    public function provides(): array
+    {
+        return [
+            FillDatabaseData::class,
+        ];
     }
 }
